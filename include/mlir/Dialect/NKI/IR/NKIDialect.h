e2cbc4447fe2c32b387ec47807ea22a5d9798d7d
Ty Klabacka <klabacka.ty@gmail.com>
2026-03-29 16:42:46 -0600

    switched to toy structure

--- include/mlir/Dialect/NKI/IR/NKIDialect.h
+++
@@ -1,2 +0,0 @@
-#include "mlir/Dialect/NKI/IR/NKIDialect.h.inc"
-#include "mlir/Dialect/NKI/IR/NKIOps.h.inc"
--- lib/Dialect/NKI/NKIDialect.cpp
+++
@@ -1,2 +0,0 @@
-#include "mlir/Dialect/NKI/IR/NKIDialect.cpp.inc"
-#include "mlir/Dialect/NKI/IR/NKIOps.cpp.inc"
