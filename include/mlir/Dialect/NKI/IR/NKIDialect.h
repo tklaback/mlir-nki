eaacd3f1f60e5dd4c844f498135abd066307352d
Ty Klabacka <klabacka.ty@gmail.com>
2026-03-29 14:45:30 -0600

    Initial commit

---
+++ include/mlir/Dialect/NKI/IR/NKIDialect.h
@@ -0,0 +1,2 @@
+#include "mlir/Dialect/NKI/IR/NKIDialect.h.inc"
+#include "mlir/Dialect/NKI/IR/NKIOps.h.inc"
---
+++ lib/Dialect/NKI/NKIDialect.cpp
@@ -0,0 +1,2 @@
+#include "mlir/Dialect/NKI/IR/NKIDialect.cpp.inc"
+#include "mlir/Dialect/NKI/IR/NKIOps.cpp.inc"
