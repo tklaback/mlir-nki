9c9d3620237ad17699e4c5bc3bfe465113326e64
Ty Klabacka <klabacka.ty@gmail.com>
2026-03-30 13:15:08 -0600

    Add Func Dialect and assembly for nki load and store

--- tools/nki-opt.cpp
+++ tools/nki-opt.cpp
@@ -1,9 +1,10 @@
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "nki/Dialect.h"
 
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
-  registry.insert<mlir::nki::NKIDialect>();
+  registry.insert<mlir::nki::NKIDialect, mlir::func::FuncDialect>();
   return mlir::asMainReturnCode(mlir::MlirOptMain(
       argc, argv, "NKI Optimizer", registry));
 }
