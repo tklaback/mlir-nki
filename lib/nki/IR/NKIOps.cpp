cb425fe4be3c30ea0601fd1efa1174e33c2e5b62
Ty Klabacka <klabacka.ty@gmail.com>
2026-03-31 17:02:13 -0600

    Add NKIOpsDialect.cpp.inc

--- lib/nki/IR/NKIOps.cpp
+++ lib/nki/IR/NKIOps.cpp
@@ -1,9 +1,8 @@
 #include "nki/IR/NKIOps.h"
+#include "nki/IR/NKIOpsDialect.cpp.inc"
+
 using namespace mlir;
 using namespace mlir::nki;
-
-#define GET_DIALECT_DEFS
-#include "nki/IR/NKIOpsDialect.cpp.inc"
 
 #define GET_OP_CLASSES
 #include "nki/IR/NKIOps.cpp.inc"
