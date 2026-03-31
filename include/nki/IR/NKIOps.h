2220f6d7d03db63912c0887559fbf233e80af2df
Ty Klabacka <klabacka.ty@gmail.com>
2026-03-31 16:57:59 -0600

    Add ops

--- include/nki/IR/NKIOps.h
+++ include/nki/IR/NKIOps.h
@@ -2,8 +2,14 @@
 #define NKI_DIALECT_H
 
 #include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/OpImplementation.h"
 
-#define GET_DIALECT_DEFS
+#include "nki/IR/NKIOpsDialect.h.inc"
+
+#define GET_OP_CLASSES
 #include "nki/IR/NKIOps.h.inc"
 
 #endif // NKI_DIALECT_H
--- lib/nki/IR/NKIOps.cpp
+++ lib/nki/IR/NKIOps.cpp
@@ -1,11 +1,16 @@
 #include "nki/IR/NKIOps.h"
-
 using namespace mlir;
 using namespace mlir::nki;
 
 #define GET_DIALECT_DEFS
+#include "nki/IR/NKIOpsDialect.cpp.inc"
+
+#define GET_OP_CLASSES
 #include "nki/IR/NKIOps.cpp.inc"
 
 void mlir::nki::NKIDialect::initialize() {
-  // Operations will be registered here once defined
+  addOperations<
+    #define GET_OP_LIST
+    #include "nki/IR/NKIOps.cpp.inc"
+  >();
 }
