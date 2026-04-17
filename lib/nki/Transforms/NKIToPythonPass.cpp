50b2b26989a92ad5d5c6bd4cb7edb79afd776ef0
Ty Klabacka <klabacka.ty@gmail.com>
2026-04-16 22:08:23 -0600

    Add emit methods

--- lib/nki/Transforms/NKIToPythonPass.cpp
+++ lib/nki/Transforms/NKIToPythonPass.cpp
@@ -1,6 +1,10 @@
+#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/Operation.h"
+#include "nki/IR/NKIOps.h"
 #include "nki/Transforms/Passes.h"
 
 #include "llvm/Support/raw_ostream.h"
@@ -17,20 +21,64 @@ struct NKIToPythonPass : public impl::NKIToPythonPassBase<NKIToPythonPass> {
     return llvm::outs() << std::string(indentLevel * 4, ' ');
   }
 
+  void emitFunc(func::FuncOp op, const WalkStage &stage) {
+    if (stage.isBeforeAllRegions()) {
+      indent() << "@nki.jit\n";
+      indent() << "def " << op.getName() << "(";
+      auto args = op.getArguments();
+      for (unsigned i = 0; i < args.size(); ++i) {
+        if (i > 0) llvm::outs() << ", ";
+        llvm::outs() << "arg" << i;
+      }
+      llvm::outs() << "):\n";
+      indentLevel++;
+      indent() << "pass";
+    } else if (stage.isAfterAllRegions()) {
+      indentLevel--;
+      llvm::outs() << "\n";
+    }
+  }
+
+  void emitConstant(arith::ConstantOp op, const WalkStage &stage) {
+    if (!stage.isBeforeAllRegions()) return;
+    // TODO: emit constant
+  }
+
+  void emitFor(scf::ForOp op, const WalkStage &stage) {
+    if (stage.isBeforeAllRegions()) {
+      // TODO: emit for loop header
+      indentLevel++;
+    } else if (stage.isAfterAllRegions()) {
+      indentLevel--;
+    }
+  }
+
+  void emitLoad(nki::LoadOp op, const WalkStage &stage) {
+    if (!stage.isBeforeAllRegions()) return;
+    // TODO: emit nl.load
+  }
+
+  void emitStore(nki::StoreOp op, const WalkStage &stage) {
+    if (!stage.isBeforeAllRegions()) return;
+    // TODO: emit nl.store
+  }
+
   void runOnOperation() override {
     llvm::outs() << "import neuronxcc.nki as nki\n";
     llvm::outs() << "import neuronxcc.nki.language as nl\n";
     llvm::outs() << "import numpy as np\n\n";
 
-    getOperation()->walk([this](func::FuncOp func, const WalkStage &stage) {
-      if (stage.isBeforeAllRegions()) {
-        indent() << "@nki.jit\n";
-        indent() << "def " << func.getName() << "():\n";
-        indentLevel++;
-        indent() << "pass\n";
-      } else if (stage.isAfterAllRegions()) {
-        indentLevel--;
-      }
+    getOperation()->walk([this](Operation *op, const WalkStage &stage) {
+      if (auto func = dyn_cast<func::FuncOp>(op))
+        emitFunc(func, stage);
+      else if (auto konst = dyn_cast<arith::ConstantOp>(op))
+        emitConstant(konst, stage);
+      else if (auto scfFor = dyn_cast<scf::ForOp>(op))
+        emitFor(scfFor, stage);
+      else if (auto load = dyn_cast<nki::LoadOp>(op))
+        emitLoad(load, stage);
+      else if (auto store = dyn_cast<nki::StoreOp>(op))
+        emitStore(store, stage);
     });
   }
 };
